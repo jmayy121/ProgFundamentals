ed0f76b1a30060b4b766fb2a121a48a6b6c77349
Juliana <36381288+jmayy121@users.noreply.github.com>
2018-02-19 17:14:28 -0500

    Add files
    Adding files corresponding with Symbolizer, Collector, Operator

---
+++ SymbOp.py
@@ -0,0 +1,11 @@
+user_input=input("type a string: ")
+if 'dog' in user_input:
+    print('Yes')
+else:
+    print('No')
+print (10.0 + 15)
+print ( 3**5)
+user_input2=input("type a number between 0 and 3")
+my_list = ['dog', 'pony', 2, 73]
+y=int(user_input2)
+print(my_list[y])
